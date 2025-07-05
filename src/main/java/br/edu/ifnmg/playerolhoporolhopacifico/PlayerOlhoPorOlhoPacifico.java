4dc05c9d40101ddc02b1e6af1f36e60a9656909a
marti <marti@DESKTOP-7TLLFG6>
2025-07-05 16:18:37 -0300

    Player Olho por Olho Pacifista Funcional

---
+++ src/main/java/br/edu/ifnmg/playerolhoporolhopacifico/PlayerOlhoPorOlhoPacifico.java
@@ -0,0 +1,47 @@
+package br.edu.ifnmg.playerolhoporolhopacifico;
+
+import io.github.guisso.meleeinterface.Decision;
+import io.github.guisso.meleeinterface.IPlayer;
+
+/**
+ *
+ * @author marti
+ */
+public class PlayerOlhoPorOlhoPacifico implements IPlayer{
+
+    private Decision penultimaJogadaOponente = Decision.COOPERATE;
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    @Override
+    public String getDeveloperName() {
+        return "Allan Martins";
+    }
+
+    @Override
+    public String getEngineName() {
+        return "Olho por Olho Pacifico";
+    }
+
+    @Override
+    public Decision makeMyMove(Decision opponentPreviousMove) {
+        Decision minhaDecisao;
+        
+        if(opponentPreviousMove == Decision.NONE){
+            minhaDecisao = Decision.COOPERATE;
+        }
+            
+        else if(opponentPreviousMove == Decision.DEFECT && penultimaJogadaOponente == Decision.DEFECT){
+            minhaDecisao = Decision.DEFECT;
+        }
+        
+        else{
+            minhaDecisao = Decision.COOPERATE;
+        }
+        
+        penultimaJogadaOponente = opponentPreviousMove;
+        return minhaDecisao;
+    }
+}
